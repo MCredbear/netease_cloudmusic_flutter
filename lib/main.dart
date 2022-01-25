af103039ef80776147342ec329d083538bce1484
MCredbear <2363764471@qq.com>
2022-01-25 23:23:57 +0800

    still building home page

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,8 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+
+import 'CustomTestButton.dart';
 
 void main() {
   runApp(const App());
@@ -54,52 +58,285 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
       body: Center(
-        child: SizedBox(
-          width: double.infinity,
-          //height: double.infinity,
-          child: Column(
-            mainAxisSize: MainAxisSize.max,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Expanded(
-                flex: 3,
-                child: Stack(
-                  children: [
-                    SizedBox(
-                      width: double.infinity,
-                      height: double.infinity,
-                      child: TextButton(onPressed: () {}, child: Text('')),
-                    ),
-
-                    Positioned(
-                      top: 50,
-                      left: 50,
-                      child: SizedBox(
-                        width: 50,
-                        height: 50,
-                        child: Image.network(
-                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg'),
+          child: ListView(
+        children: <Widget>[
+          SizedBox(
+            width: double.infinity,
+            height: 150,
+            child: Card(
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                children: [
+                  CustomTextButton.icon(
+                      style: const ButtonStyle(
+                          splashFactory: NoSplash.splashFactory),
+                      onPressed: () {},
+                      icon: const Icon(Icons.keyboard_arrow_right_sharp),
+                      label: Text(
+                        " 用户信息",
+                        textScaleFactor: 1.2,
+                      )),
+                  Expanded(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 8, vertical: 2),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        crossAxisAlignment: CrossAxisAlignment.end,
+                        children: [
+                          Card(
+                            clipBehavior: Clip.antiAlias,
+                            child: Image.network(
+                                'https://p4.music.126.net/8B-aaaDhCw_WYD8u-h3Vfg==/109951163124857862.jpg'),
+                          ),
+                          Expanded(
+                            child: Column(
+                              mainAxisSize: MainAxisSize.max,
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Padding(
+                                  padding: const EdgeInsets.only(top: 4),
+                                  child: Flex(
+                                    direction: Axis.horizontal,
+                                    children: [
+                                      Card(
+                                        shape: const StadiumBorder(
+                                            side: BorderSide(
+                                          style: BorderStyle.none,
+                                        )),
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 10),
+                                          child: Text(
+                                            'VIP：无 剩余时间：0',
+                                            textScaleFactor: 1.2,
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                      ),
+                                      Card(
+                                        shape: const StadiumBorder(
+                                            side: BorderSide(
+                                          style: BorderStyle.none,
+                                        )),
+                                        child: Padding(
+                                          padding: const EdgeInsets.symmetric(
+                                              horizontal: 10),
+                                          child: Text(
+                                            '等级：7',
+                                            textScaleFactor: 1.2,
+                                            textAlign: TextAlign.center,
+                                          ),
+                                        ),
+                                      ),
+                                    ],
+                                  ),
+                                ),
+                                Padding(
+                                  padding: const EdgeInsets.symmetric(
+                                      horizontal: 10, vertical: 0),
+                                  child: Text(
+                                    "MCredbear",
+                                    textAlign: TextAlign.left,
+                                    textScaleFactor: 2,
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ),
+                        ],
                       ),
                     ),
-                    //Text('MCredbear')
-                  ],
-                ),
+                  )
+                ],
               ),
-              Expanded(
-                flex: 1,
-                child: Divider(
-                  thickness: 1,
-                ),
-              ),
-              Expanded(
-                  flex: 1,
-                  child: Card(
-                    child: Text('114514'),
-                  )),
-            ],
+            ),
           ),
-        ),
-      ),
+          const Divider(
+            thickness: 1,
+          ),
+          CustomTextButton.icon(
+            //style: ButtonStyle(splashFactory: NoSplash.splashFactory),
+            onPressed: () {},
+            icon: const Icon(Icons.keyboard_arrow_right_sharp),
+            label: Text(
+              " 本地音乐",
+              textScaleFactor: 1.2,
+            ),
+          ),
+          Card(
+            child: SizedBox(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                //mainAxisSize: MainAxisSize.min,
+                //width: double.infinity,
+                //height: 100,
+                children: [
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+          const Divider(
+            thickness: 1,
+          ),
+          CustomTextButton.icon(
+            //style: ButtonStyle(splashFactory: NoSplash.splashFactory),
+            onPressed: () {},
+            icon: const Icon(Icons.keyboard_arrow_right_sharp),
+            label: Text(
+              " 历史记录",
+              textScaleFactor: 1.2,
+            ),
+          ),
+        ],
+      )),
     );
   }
 }
