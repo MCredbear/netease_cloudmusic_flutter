09ea848020ed51eca277dff584c6a14fe32960d5
MCredbear <2363764471@qq.com>
2022-01-26 10:54:31 +0800

    added song list

--- lib/main.dart
+++ lib/main.dart
@@ -11,12 +11,12 @@ void main() {
 class App extends StatelessWidget {
   const App({Key? key}) : super(key: key);
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Netease CloudMusic Flutter',
       theme: ThemeData.dark(),
+      //themeMode: ThemeMode.system,
       home: const HomePage(),
     );
   }
@@ -25,15 +25,6 @@ class App extends StatelessWidget {
 class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
   @override
   State<HomePage> createState() => _HomePageState();
 }
@@ -41,12 +32,6 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
         title: const Text("Netease CloudMusic"),
@@ -62,20 +47,21 @@ class _HomePageState extends State<HomePage> {
         children: <Widget>[
           SizedBox(
             width: double.infinity,
-            height: 150,
+            height: 100,
             child: Card(
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
-                  CustomTextButton.icon(
-                      style: const ButtonStyle(
-                          splashFactory: NoSplash.splashFactory),
-                      onPressed: () {},
-                      icon: const Icon(Icons.keyboard_arrow_right_sharp),
-                      label: Text(
-                        " 用户信息",
-                        textScaleFactor: 1.2,
-                      )),
+                  /*CustomTextButton.icon( //too ugly
+                    style: const ButtonStyle(
+                        splashFactory: NoSplash.splashFactory),
+                    onPressed: () {},
+                    icon: const Icon(Icons.keyboard_arrow_right_sharp),
+                    label: Text(
+                      " 用户信息",
+                      textScaleFactor: 1.2,
+                    ),
+                  ),*/
                   Expanded(
                     child: Padding(
                       padding: const EdgeInsets.symmetric(
@@ -159,10 +145,183 @@ class _HomePageState extends State<HomePage> {
           CustomTextButton.icon(
             //style: ButtonStyle(splashFactory: NoSplash.splashFactory),
             onPressed: () {},
-            icon: const Icon(Icons.keyboard_arrow_right_sharp),
+            icon: const Icon(
+              Icons.keyboard_arrow_right_sharp,
+              size: 30,
+            ),
             label: Text(
               " 本地音乐",
-              textScaleFactor: 1.2,
+              textScaleFactor: 2,
+            ),
+          ),
+          /*Card(
+            child: SizedBox(
+              height: 100,
+              child: ListView(
+                scrollDirection: Axis.horizontal,
+                shrinkWrap: true,
+                children: [
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                  Card(
+                    child: InkWell(
+                      borderRadius: BorderRadius.circular(4),
+                      onTap: () {},
+                      child: Padding(
+                        padding: const EdgeInsets.all(1),
+                        child: Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            //width: 50,
+                            //height: 50,
+                          ),
+                        ),
+                      ),
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),*/
+          const Divider(
+            thickness: 1,
+          ),
+          CustomTextButton.icon(
+            //style: ButtonStyle(splashFactory: NoSplash.splashFactory),
+            onPressed: () {},
+            icon: const Icon(
+              Icons.keyboard_arrow_right_sharp,
+              size: 30,
+            ),
+            label: Text(
+              " 历史记录",
+              textScaleFactor: 2,
             ),
           ),
           Card(
@@ -323,18 +482,61 @@ class _HomePageState extends State<HomePage> {
               ),
             ),
           ),
-          const Divider(
+          Divider(
             thickness: 1,
           ),
           CustomTextButton.icon(
-            //style: ButtonStyle(splashFactory: NoSplash.splashFactory),
             onPressed: () {},
-            icon: const Icon(Icons.keyboard_arrow_right_sharp),
+            icon: const Icon(
+              Icons.keyboard_arrow_right_sharp,
+              size: 30,
+            ),
             label: Text(
-              " 历史记录",
-              textScaleFactor: 1.2,
+              ' 我的歌单',
+              textScaleFactor: 2,
             ),
           ),
+          Card(
+            child: SizedBox(
+              width: double.infinity,
+              child: ListView(
+                scrollDirection: Axis.vertical,
+                shrinkWrap: true,
+                children: [
+                  InkWell(
+                    onTap: () {},
+                    splashFactory: NoSplash.splashFactory,
+                    borderRadius: BorderRadius.all(Radius.circular(4)),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.center,
+                      children: [
+                        Card(
+                          elevation: 0,
+                          clipBehavior: Clip.antiAlias,
+                          child: Image.network(
+                            'https://p3.music.126.net/hTceeVgvD7zBgy5kLeMr6w==/109951163124863182.jpg',
+                            width: 50,
+                            height: 50,
+                          ),
+                        ),
+                        Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              '我喜欢的音乐',
+                              textScaleFactor: 1.5,
+                            ),
+                            Text('114514首'),
+                          ],
+                        ),
+                      ],
+                    ),
+                  )
+                ],
+              ),
+            ),
+          )
         ],
       )),
     );
