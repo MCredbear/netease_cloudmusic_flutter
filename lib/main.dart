99f130db42d787c1ba24ae36b6b4e9bf5313b020
MCredbear <2363764471@qq.com>
2022-02-28 23:11:45 +0800

    added recent_songs_page

--- lib/main.dart
+++ lib/main.dart
@@ -2,16 +2,16 @@ import 'package:flutter/material.dart';
 import 'package:flutter_mobx/flutter_mobx.dart';
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:assets_audio_player/assets_audio_player.dart';
-import 'package:netease_cloudmusic_flutter/api/song_url.dart';
+import 'package:netease_cloudmusic_flutter/recent_songs_page.dart';
+import 'package:netease_cloudmusic_flutter/stores/recent_songs_store.dart';
 import 'package:netease_cloudmusic_flutter/widgets/CustomTestButton.dart';
-import 'package:netease_cloudmusic_flutter/api/record_recent_song.dart';
 import 'package:netease_cloudmusic_flutter/api/user_account.dart';
 import 'package:netease_cloudmusic_flutter/api/user_playlist.dart';
-import 'package:netease_cloudmusic_flutter/stores/recent_songs_store.dart';
 import 'package:netease_cloudmusic_flutter/stores/user_playlists_store.dart';
 import 'package:netease_cloudmusic_flutter/stores/user_profile_store.dart';
 
 import 'login_page.dart';
+import 'widgets/recent_songs_list_view.dart';
 
 void main() {
   runApp(const App());
@@ -181,7 +181,10 @@ class _HomePageState extends State<HomePage> {
                 children: [
                   CustomTextButton.icon(
                     onPressed: () {
-                      recordRecentSong(limit: "300");
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => const RecentSongPage()));
                     },
                     icon: const Icon(
                       Icons.keyboard_arrow_right_sharp,
@@ -198,7 +201,13 @@ class _HomePageState extends State<HomePage> {
                     height: 0,
                     thickness: 1,
                   ),
-                  const RecentSongsListView(),
+                  RecentSongsListView(
+                    count: (storeRecentSongs.recentSongs.length >= 5)
+                        ? 5
+                        : storeRecentSongs.recentSongs.length,
+                    physics: const NeverScrollableScrollPhysics(),
+                    shrinkWrap: true,
+                  ),
                 ],
               ),
             ),
@@ -456,118 +465,6 @@ class _SubscribedPlaylistListViewState
                   ],
                 ),
               ),
-            ],
-          ),
-        ),
-      ),
-    );
-  }
-}
-
-class RecentSongsListView extends StatefulWidget {
-  const RecentSongsListView({
-    Key? key,
-  }) : super(key: key);
-
-  @override
-  State<RecentSongsListView> createState() => _RecentSongsListViewState();
-}
-
-class _RecentSongsListViewState extends State<RecentSongsListView> {
-  @override
-  void initState() {
-    super.initState();
-    recordRecentSong(limit: "300");
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Observer(
-      builder: (_) => ListView.builder(
-        physics: const NeverScrollableScrollPhysics(),
-        shrinkWrap: true,
-        itemCount: (storeRecentSongs.recentSongs.length > 5)
-            ? 5
-            : storeRecentSongs.recentSongs.length,
-        itemBuilder: (context, index) => InkWell(
-          onTap: () async {
-            player.pause();
-            print(await songUrl(storeRecentSongs.recentSongs[index].id));
-            if (await songUrl(storeRecentSongs.recentSongs[index].id) != '')
-              try {
-                await player.open(
-                  Audio.network(
-                      await songUrl(storeRecentSongs.recentSongs[index].id),
-                      metas: Metas(
-                          title: storeRecentSongs.recentSongs[index].name,
-                          artist: storeRecentSongs.recentSongs[index]
-                              .combinedArtistsName(),
-                          image: MetasImage.network(
-                              storeRecentSongs.recentSongs[index].coverUrl)),
-                      cached: true),
-                  showNotification: true,
-                );
-              } catch (t) {}
-            else
-              print("被网易云ban了");
-          },
-          splashFactory: NoSplash.splashFactory,
-          borderRadius: (index ==
-                  ((storeRecentSongs.recentSongs.length > 5)
-                      ? 4
-                      : storeRecentSongs.recentSongs.length - 1))
-              ? const BorderRadius.vertical(bottom: Radius.circular(4))
-              : const BorderRadius.all(Radius.zero),
-          child: Row(
-            children: [
-              Padding(
-                padding: const EdgeInsets.fromLTRB(8, 0, 8, 0),
-                child: Card(
-                  elevation: 0,
-                  clipBehavior: Clip.antiAlias,
-                  child: Observer(
-                    builder: (_) => CachedNetworkImage(
-                      imageUrl: storeRecentSongs.recentSongs[index].coverUrl,
-                      fit: BoxFit.fill,
-                      width: 40,
-                      height: 40,
-                    ),
-                  ),
-                ),
-              ),
-              Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Observer(
-                      builder: (_) => RichText(
-                          textScaleFactor: 1.2,
-                          overflow: TextOverflow.ellipsis,
-                          text: TextSpan(children: [
-                            TextSpan(
-                                text: storeRecentSongs.recentSongs[index].name),
-                            TextSpan(
-                              text: (storeRecentSongs
-                                      .recentSongs[index].alias.isNotEmpty)
-                                  ? "  (" +
-                                      storeRecentSongs
-                                          .recentSongs[index].alias[0] +
-                                      ")"
-                                  : "",
-                              style: const TextStyle(color: Colors.white54),
-                            )
-                          ])),
-                    ),
-                    Observer(
-                        builder: (_) => Text(
-                              storeRecentSongs.recentSongs[index]
-                                  .combinedArtistsName(),
-                              overflow: TextOverflow.ellipsis,
-                            )),
-                  ],
-                ),
-              )
             ],
           ),
         ),
